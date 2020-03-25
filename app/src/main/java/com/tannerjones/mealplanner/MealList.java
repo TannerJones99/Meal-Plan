65b28c99e15f61a03e40fe980242cd709708e8a9
Jones, Tanner <TJONES99@MY.APSU.EDU>
2020-03-24 22:18:04 -0500

    gave add meal function

--- app/src/main/java/com/tannerjones/mealplanner/MealList.java
+++ app/src/main/java/com/tannerjones/mealplanner/MealList.java
@@ -4,6 +4,8 @@ package com.tannerjones.mealplanner;
 This class will read meals from a file and handle the list of meals.
  */
 
+import android.content.Context;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -11,17 +13,15 @@ public class MealList implements Serializable {
     private ArrayList<Meal> meals;
 
     public MealList(){
-        UpdateMeals();
-    }
-
-    public boolean UpdateMeals(){
-        // Will update the list of meals from the file whenever called. Returns true if successfully update. Returns false if not.
         meals = new ArrayList<>();
-
-        return true;
     }
 
     public ArrayList<Meal> getMeals() {
         return meals;
+    }
+
+    public void addMealToList(Meal meal, ArrayList<MealPlan> plans, Context context){
+        meals.add(meal);
+        new MealSave().updateMealPlans(plans, context);
     }
 }
